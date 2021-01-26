aec559d8281043e76e79b34622a2500f9b9670e5
igotbitches <check.point9917@yandex.ru>
2021-01-26 14:14:20 +0300

    feat: create function for sort matrix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
-
-// You should implement your task here.
-
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+    if (!matrix) {
+        return [];
+    }
+    let array = new Array();
+    for (let i = 0; i < matrix.length; i++) {
+        if (i % 2 === 1)
+            for (let j = matrix[i].length - 1; j >= 0; j--) {
+                if (!(matrix[i][j] === undefined)) array.push(matrix[i][j]);
+            }
+        else {
+            for (let j = 0; j < matrix[i].length; j++) {
+                if (!(matrix[i][j] === undefined)) array.push(matrix[i][j]);
+            }
+        }
+    }
+    return array;
+};
